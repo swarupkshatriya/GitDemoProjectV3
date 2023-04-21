4bac8c9b6ad1c9d01c99b8758d6f5426d7a78741
swarupkshatriya <swarup.kshatriya@gmail.com>
2023-04-21 16:59:53 +0530

    committing to local repo

---
+++ src/TestingDemo3/TestingClass3.java
@@ -0,0 +1,10 @@
+package TestingDemo3;
+
+public class TestingClass3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is very useful");
+	}
+
+}
